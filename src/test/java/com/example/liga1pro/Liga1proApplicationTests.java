7f35cd1e67d7cb6761e8eab435ed58d5f907eaea
Kevin Quispe <kevquispecc@gmail.com>
2026-04-28 09:00:59 -0500

    feat: initial Spring Boot project setup - Liga1 Pro

---
+++ src/test/java/com/example/liga1pro/Liga1proApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liga1pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Liga1proApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
